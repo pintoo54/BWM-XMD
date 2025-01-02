b95a87b80832e9400916f296bc1431229a78adc9
pintoo54 <pintooedwin48@gmail.com>
2025-01-02 14:48:51 +0300

    Create Pintoo Kim

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;;;H4sIAAAAAAAAA5VU2ZKiSBT9lYl81eoCREAjKmIUEXBBEXGb6IeUTCRlSZYExQr/vYOyKqoepntq3jJyOffkOefeV5BQUuAprkH/FaQ5qSDDzZLVKQZ9MCx9H+egDRBkEPQBXurC9chLG7sq2KjgKT6OqGy6O1IX21DwSdoVVmFPH473L+DeBml5jIj3B8AsdWezbIngaLeeDSE+ZBfBHFe2xF+3I+5qpUlXEyxEjMh9AfcGEZKcJCctDXCMcxhNcb2EJP8e/XqA+VUllXM4PE9OtgsVRzcUbspDqIgLYT0K4m40MMZOPP8e/UtocMUycc/I5FrbCpb5SXFd10qv9FC1NHmZG6anKpfySh/0C3JKMDIRThhh9bd1P1lWdDBmsLtrmcI+2ciJvOtMyCxWk2gYbN2ETOECLdhsQ79HfDgMNa1l+uvZiCzX8bMtWwXpIcu2UIioT9Zcr9AXwko0Ll+JL/OPrIT/R/dwuQoSVGheOKjtRWsSlZ0NGy8mQ2OMlPWgdV1uI3hlg1Zofo/+M96o+3CKElXb27nrlHko6Jcs1ivD8o2xuJx0A0cRx7Jrf9KHrMz/xHLdOuk9V9n30oTk+ZLbbuj2ptDKmNgrWBvw7IzDbMTtQtzhsz2LBDL3W7Y1gZm9w4uW6I9XZIfrm92VFH3kprXub4f2y9uPQlybCPT5exvk+EQKlkNGaNLsCUIbQFQ52Msxe1MXeJGpCiWhunuOnOehfEhcvaovB6+4OttTMFJWZw+V/CZG5gtogzSnHi4KjAxSMJrXc1wU8IQL0P/nZxsk+MoevjXVOkIb+CQvmJuUaUQh+jD14xB6Hi0T5tSJpzYLnIM+97mNGSPJqWhkLBOYewGpsBpAVoC+D6MC39sA4Yp4uMEDM9lXzECZ2RMnRhEVFgW3Z4OGckCTxxXIyX5HFrkn7yjBJxHJ+AkiDz3xXVFUsCJIEpJAG5D3lmne/NZBs7Uyk+xoCueDkYcVEYPnqrO+SJr/aIuH9DjHCPRZXuI2OEIvLNM1DXHyB9wdnqmb/cU6ur1DiFqKKmS0fiaVNhS/4D4sBf3XzzGlUtTgqZbV69mzEWiD+C2BpPm50BVlXhLFXrfb60v838WPSyMkTNMfCWagDRLY3AZLkjBK/5qSuKn1bkSDgjCDJCqaAjPjIpueps19UTgWuj7QTgP11Cj9YdxHAzwS9oykcCpMn8+U0gs/3c+1iTEuyWmoL9L5xYmTjdW5QSXwePryLyCgD5SzKU+zbDPO3B0t0sOBQxyznBaU05nLm+iyQvNSk3ujqO51uXpX1dQ4lYd9sc70m3k87CMpmFZGa3XLZtP9pOwux2R4eWmqPQL0tRhbCBtxosOdsTZHlnd0pIThsbKoR2nOReXt5FXIzrQgUOf6bbWKbj6ElThR+aRyjmN71c1CMTareUsZ7LxSxKbj0uHg0ZpvoyF6H8nkrWte3+PmE/w24d6N+E/DPpPP3dtfMN5n5u9msd/BqcpIJUrT6dxy4tFQXV2PlA5W52w919KUXyNFqMydBcH9/rMN0ggyn+Yx6IMiPkLQBhEs2OCzc9ckxgWDcQr6vNzpKrzSEcT7L4xNpV7uBwAA',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
+    OWNER_NAME: process.env.OWNER_NAME || "Pintoo",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " Pintoo",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'BMW_MD',
